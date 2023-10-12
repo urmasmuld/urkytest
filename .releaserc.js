260d4b58dafa0c925d5a6d141758d8a49ebe4d98
urmasmuld <urmas.muldmaa@gmail.com>
2023-10-12 20:49:31 +0300

    fix: Testikas

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,13 @@
 module.exports = {
     branches: ['main'],
     plugins: [
-      "@semantic-release/commit-analyzer",
+      ["@semantic-release/commit-analyzer", {
+      releaseRules: [
+        { type: 'fix', release: 'patch' },
+        { type: 'feat', release: 'minor' },
+        { type: 'rel', release: 'major' },
+      ],
+    }],
       "@semantic-release/release-notes-generator",
       "@semantic-release/changelog",
       ["@semantic-release/npm", {
